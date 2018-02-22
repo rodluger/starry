0cc8c668f2a3a2140b4254e4d4586830d207844e
Rodrigo Luger <rodluger@gmail.com>
2018-02-22 10:19:37 -0800

    woot

---
+++ tex/figures/earth_phasecurve.py
@@ -0,0 +1,18 @@
+"""Earth phase curve example."""
+from starry import starry
+import matplotlib.pyplot as pl
+import numpy as np
+
+# Generate a sample starry map
+y = starry(10, 'earth')
+y.rotate([0, 1, 0], -np.pi)
+
+# Compute the phase curve
+npts = 100
+fig, ax = pl.subplots(1, figsize=(9, 9))
+theta = np.linspace(0, 2 * np.pi, npts, endpoint=False)
+F = y.flux(res=300, u=[0,1,0], theta=theta)
+ax.plot(theta, F)
+
+# Save
+pl.savefig('earth_phasecurve.pdf', bbox_inches='tight')
