c81f02c6e12c8e13584736df1be99c88e78d40e0
Rodrigo Luger <rodluger@gmail.com>
2018-02-22 11:09:53 -0800

    tweakz

--- tex/figures/earth_phasecurve.py
+++ tex/figures/earth_phasecurve.py
@@ -1,27 +1,44 @@
 """Earth phase curve example."""
 from starry import starry
 import matplotlib.pyplot as pl
+import matplotlib
+matplotlib.rc('text', usetex=True)
 import numpy as np
 
-# Generate a sample starry map
-y = starry(10, 'earth')
-y.rotate([0, 1, 0], -np.pi)
-
-# Compute the phase curve
+# Set up the plot
 npts = 100
-fig, ax = pl.subplots(1, figsize=(9, 9))
-theta = np.linspace(0, 2 * np.pi, npts, endpoint=False)
-F = y.flux(res=300, u=[0, 1, 0], theta=theta)
-ax.plot(theta, F)
+fig, ax = pl.subplots(1, figsize=(12, 7))
+theta = np.linspace(0, 2 * np.pi, npts, endpoint=True)
+total = np.zeros(npts, dtype=float)
 
 # Compute the phase curves for each continent
-for continent in ['northamerica', 'southamerica',
-                  'europe', 'asia', 'oceania',
-                  'antarctica', 'africa']:
+base = 116.5
+norm = 180
+continents = ['asia', 'africa',
+              'southamerica', 'northamerica', 'oceania',
+              'europe', 'antarctica']
+labels = ['Asia', 'Africa', 'S. America',
+          'N. America', 'Oceania', 'Europe',
+          'Antactica']
+for continent, label in zip(continents, labels):
     y = starry(10, continent)
     y.rotate([0, 1, 0], -np.pi)
     F = y.flux(res=300, u=[0, 1, 0], theta=theta)
-    ax.plot(theta, F)
+    F = (F - base) / norm
+    total += F
+    ax.plot(theta * 180 / np.pi - 180, F, label=label)
+
+# Plot the total phase curve
+ax.plot(theta * 180 / np.pi - 180, total, 'k-', label='Total')
+
+# Appearance
+ax.set_xlim(-180, 180)
+ax.set_xticks([-180, -135, -90, -45, 0, 45, 90, 135, 180])
+ax.legend(loc='best', fontsize=12)
+ax.set_xlabel('Sub-observer longitude (deg)', fontsize=18)
+ax.set_ylabel('Normalized flux', fontsize=18)
+for tick in ax.get_xticklabels() + ax.get_yticklabels():
+    tick.set_fontsize(15)
 
 # Save
 pl.savefig('earth_phasecurve.pdf', bbox_inches='tight')
