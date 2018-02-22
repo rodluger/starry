e74c3f1c22b5a6fc7b47cc773b1bd93775112ebb
Rodrigo Luger <rodluger@gmail.com>
2018-02-22 14:18:44 -0800

    oh travis

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -8,9 +8,8 @@ hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda info -a
-conda create --yes -n test python=$PYTHON_VERSION
-source activate test
-conda install -c conda-forge numpy=$NUMPY_VERSION scipy matplotlib sympy mpmath setuptools pytest pytest-cov pip healpy PIL pkgw-forge tectonic
+conda install -c conda-forge numpy=$NUMPY_VERSION scipy matplotlib sympy mpmath setuptools pytest pytest-cov pip healpy PIL
+conda install -c pkgw-forge tectonic
 
 # Build the code
 python setup.py install
