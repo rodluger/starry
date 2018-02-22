1a7c7a6bf3fb571cb4e49ea72324571cb3f4d953
Rodrigo Luger <rodluger@gmail.com>
2018-02-21 16:16:57 -0800

    healpix functionality

---
+++ starry/maps.py
@@ -0,0 +1,64 @@
+"""Map fitting utilities for STARRY."""
+from .rotation import R
+import numpy as np
+import healpy as hp
+from PIL import Image
+from matplotlib.image import pil_to_array
+import os
+
+
+__all__ = ["image2map"]
+
+
+def image2map(image, lmax=10):
+    """Return a map vector corresponding to a lat-long map image."""
+    # If image doesn't exist, check for it in maps directory
+    if not os.path.exists(image):
+        dn = os.path.dirname
+        image = os.path.join(dn(dn(os.path.abspath(__file__))),
+                             "maps", image + ".jpg")
+        if not os.path.exists(image):
+            raise ValueError("Image file not found.")
+
+    # Get the image array
+    grayscale_pil_image = Image.open(image).convert("L")
+    image_array = pil_to_array(grayscale_pil_image)
+
+    # Figure out a reasonable number of sides
+    # TODO: Not optimized!
+    npix = image_array.shape[0] * image_array.shape[1]
+    nside = 2
+    while hp.nside2npix(nside) * 8 < npix:
+        nside *= 2
+
+    # Convert to a healpix map
+    theta = np.linspace(0, np.pi, image_array.shape[0])[:, None]
+    phi = np.linspace(-np.pi, np.pi, image_array.shape[1])[::-1]
+    pix = hp.ang2pix(nside, theta, phi, nest=False)
+    healpix_map = np.zeros(hp.nside2npix(nside), dtype=np.double)
+    healpix_map[pix] = image_array
+
+    # Get the complex spherical harmonic coefficients
+    alm = hp.sphtfunc.map2alm(healpix_map, lmax=lmax)
+
+    # Convert them to real coefficients
+    ylm = np.zeros(lmax ** 2 + 2 * lmax + 1, dtype='float')
+    i = 0
+    for l in range(0, lmax + 1):
+        for m in range(-l, l + 1):
+            j = hp.sphtfunc.Alm.getidx(lmax, l, np.abs(m))
+            if m < 0:
+                ylm[i] = np.sqrt(2) * (-1) ** m * alm[j].imag
+            elif m == 0:
+                ylm[i] = alm[j].real
+            else:
+                ylm[i] = np.sqrt(2) * (-1) ** m * alm[j].real
+            i += 1
+
+    # We need to apply some rotations to return
+    # to the original orientation
+    ylm = np.dot(R(lmax, [1, 0, 0], 0, 1), ylm)
+    ylm = np.dot(R(lmax, [0, 0, 1], -1, 0), ylm)
+    ylm = np.dot(R(lmax, [0, 1, 0], 0, 1), ylm)
+
+    return ylm
--- starry/user.py
+++ starry/user.py
@@ -3,7 +3,9 @@ import numpy as np
 from .rotation import R, Rz
 from .basis import A, evaluate_poly, y2p
 from .integrals import S, brute
+from .maps import image2map
 import matplotlib.pyplot as pl
+from matplotlib.animation import FuncAnimation
 
 
 __all__ = ["starry"]
@@ -12,26 +14,30 @@ __all__ = ["starry"]
 class starry(np.ndarray):
     """The main STARRY interface. This is a subclass of numpy.ndarray."""
 
-    def __new__(subtype, lmax, tol=1e-15):
+    def __new__(subtype, lmax, image=None, tol=1e-15):
         """Create a new `starry` instance by subclassing ndarray."""
         # Check that lmax is a nonnegative integer
         assert type(lmax) is int, "Argument `lmax` must be an integer."
         assert lmax >= 0, "Argument `lmax` must be >= 0."
-        
+
         # Instantiate a numpy array
         shape = (lmax + 1) ** 2
         obj = super(starry, subtype).__new__(subtype, shape, float,
-                                                None, 0, None,
-                                                None)
-        
+                                             None, 0, None,
+                                             None)
+
         # Initialize with zeros
         obj[:] = 0
         obj.lmax = lmax
         obj.tol = tol
-        
+
         # Pre-compute the basis change matrix
         obj.A = A(obj.lmax)
-        
+
+        # Did the user specify an image map?
+        if image is not None:
+            obj[:] = image2map(image, lmax=lmax)
+
         return obj
 
     @property
@@ -41,7 +47,7 @@ class starry(np.ndarray):
 
     def __array_finalize__(self, obj):
         """Finalize the array (needed for ndarray subclasses)."""
-        if obj is None: 
+        if obj is None:
             return
         self.info = getattr(obj, 'info', None)
 
@@ -73,7 +79,7 @@ class starry(np.ndarray):
             l, m = lm
             n = l ** 2 + l + m
             if (l < 0) or (n >= len(self.y)) or (np.abs(m) > l):
-                raise ValueError("Invalid value for `l` and/or `m`.")  
+                raise ValueError("Invalid value for `l` and/or `m`.")
         except (TypeError, ValueError):
             n = lm
         return super(starry, self).__getitem__(n)
@@ -84,13 +90,13 @@ class starry(np.ndarray):
             l, m = lm
             n = l ** 2 + l + m
             if (l < 0) or (n >= len(self.y)) or (np.abs(m) > l):
-                raise ValueError("Invalid value for `l` and/or `m`.")  
+                raise ValueError("Invalid value for `l` and/or `m`.")
         except (TypeError, ValueError):
             n = lm
         return super(starry, self).__setitem__(n, val)
 
     def __add__(self, y):
-        """Implements addition for two `starry` objects of different sizes."""
+        """Implement addition for two `starry` objects of different sizes."""
         assert type(y) is starry, "Only `starry` instances can be added."
         if len(self) == len(y):
             z = self + y
@@ -105,7 +111,8 @@ class starry(np.ndarray):
             z.lmax = y.lmax
         return z
 
-    def flux(self, u=[0, 1, 0], theta=0, x0=None, y0=None, r=None, debug=False, res=100):
+    def flux(self, u=[0, 1, 0], theta=0, x0=None, y0=None, r=None, debug=False,
+             res=100):
         """Return the flux visible from the map."""
         # Is this an occultation?
         if (x0 is None) or (y0 is None) or (r is None):
@@ -159,7 +166,8 @@ class starry(np.ndarray):
                 b = np.inf
 
             # Rotate the map so the occultor is along the y axis
-            if (occultation) and (b > self.tol) and (not debug) and (b < 1 + r[n]):
+            if (occultation) and (b > self.tol) \
+                    and (not debug) and (b < 1 + r[n]):
                 sinvt = x0[n] / b
                 cosvt = y0[n] / b
                 R2 = Rz(self.lmax, cosvt, sinvt, tol=self.tol)
@@ -198,8 +206,8 @@ class starry(np.ndarray):
 
     def rotate(self, u=[0, 1, 0], theta=0):
         """Rotate the base map."""
-        self[:] = np.dot(R(self.lmax, u, np.cos(theta), np.sin(theta), tol=self.tol),
-                   self.y)
+        self[:] = np.dot(R(self.lmax, u, np.cos(theta), np.sin(theta),
+                           tol=self.tol), self.y)
 
     def render(self, u=[0, 1, 0], theta=0, x0=None, y0=None, r=None, res=100):
         """Return a pixelized image of the visible portion of the map."""
@@ -235,3 +243,62 @@ class starry(np.ndarray):
         ax.imshow(F, origin="lower", interpolation="none", cmap=cmap)
         ax.axis('off')
         pl.show()
+
+    def _step(self, j):
+        """Take an animation step."""
+        u = self._animation['u']
+        res = self._animation['res']
+        theta = self._animation['theta'][j]
+        x0 = self._animation['x0'][j]
+        y0 = self._animation['y0'][j]
+        r = self._animation['r'][j]
+        F = self.render(u=u, theta=theta, x0=x0, y0=y0, r=r, res=res)
+        self._img.set_data(F)
+        return self._img,
+
+    def animate(self, u=[0, 1, 0],
+                theta=np.linspace(0, 2 * np.pi, 30, endpoint=False),
+                x0=None, y0=None, r=None, res=100,
+                cmap='plasma', interval=50, gif=None,
+                fps=10, dpi=100, vmin=None, vmax=None, **kwargs):
+        """Show an animation of the rendered map."""
+        # Vectorize the inputs
+        if (x0 is None) or (y0 is None) or (r is None):
+            x0 = -1
+            y0 = -1
+            r = 0
+        x0 = np.atleast_1d(x0)
+        y0 = np.atleast_1d(y0)
+        r = np.atleast_1d(r)
+        theta = np.atleast_1d(theta)
+        npts = max(len(x0), len(y0), len(r), len(theta))
+        if len(x0) == 1:
+            x0 = np.ones(npts, dtype=float) * x0[0]
+        if len(y0) == 1:
+            y0 = np.ones(npts, dtype=float) * y0[0]
+        if len(r) == 1:
+            r = np.ones(npts, dtype=float) * r[0]
+        if len(theta) == 1:
+            theta = np.ones(npts, dtype=float) * theta[0]
+        assert len(x0) == len(y0) == len(r) == len(theta), \
+            "Invalid dimensions for `x0`, `y0`, `r`, and/or `theta`."
+        self._animation = dict(u=u,
+                               theta=theta,
+                               x0=x0,
+                               y0=y0,
+                               r=r,
+                               res=res)
+        self._fig, self._ax = pl.subplots(1, figsize=(3, 3))
+        self._ax.axis('off')
+        F = self.render(u=u, theta=theta[0], x0=x0[0], y0=y0[0], r=r[0],
+                        res=res)
+        self._img = self._ax.imshow(F, origin="lower", interpolation="none",
+                                    cmap=cmap, vmin=vmin, vmax=vmax)
+        animation = FuncAnimation(self._fig, self._step,
+                                  frames=npts,
+                                  interval=interval,
+                                  repeat=True, blit=True)
+        if gif is None:
+            pl.show()
+        else:
+            animation.save(gif, writer='imagemagick', fps=fps, dpi=dpi)
