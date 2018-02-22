a358b65c65670ae840adf95e8205b79493b8c52a
Rodrigo Luger <rodluger@gmail.com>
2018-02-22 10:40:22 -0800

    travis tweak

--- tex/figures/earth_phasecurve.py
+++ tex/figures/earth_phasecurve.py
@@ -11,8 +11,17 @@ y.rotate([0, 1, 0], -np.pi)
 npts = 100
 fig, ax = pl.subplots(1, figsize=(9, 9))
 theta = np.linspace(0, 2 * np.pi, npts, endpoint=False)
-F = y.flux(res=300, u=[0,1,0], theta=theta)
+F = y.flux(res=300, u=[0, 1, 0], theta=theta)
 ax.plot(theta, F)
+
+# Compute the phase curves for each continent
+for continent in ['northamerica', 'southamerica',
+                  'europe', 'asia', 'oceania',
+                  'antarctica', 'africa']:
+    y = starry(10, continent)
+    y.rotate([0, 1, 0], -np.pi)
+    F = y.flux(res=300, u=[0, 1, 0], theta=theta)
+    ax.plot(theta, F)
 
 # Save
 pl.savefig('earth_phasecurve.pdf', bbox_inches='tight')
