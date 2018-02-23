14db656f7ce302d878778bbb4e1a36c783afc153
Rodrigo Luger <rodluger@gmail.com>
2018-02-22 16:04:55 -0800

    sigh

--- tex/figures/earthphasecurve.py
+++ tex/figures/earthphasecurve.py
@@ -38,7 +38,7 @@ ax.set_ylim(-0.03, 1.1)
 ax.set_xticks([-180, -135, -90, -45, 0, 45, 90, 135, 180])
 ax.set_yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0])
 ax.legend(loc='best', fontsize=14, ncol=2)
-ax.set_xlabel('Sub-observer longitude', fontsize=24)
+ax.set_xlabel('Sub-observer longitude [deg]', fontsize=24)
 ax.set_ylabel('Normalized flux', fontsize=24)
 for tick in ax.get_xticklabels() + ax.get_yticklabels():
     tick.set_fontsize(22)
