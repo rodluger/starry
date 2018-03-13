64fc224b043dd59e37375a57a435e3d1ecf9bf95
Rodrigo Luger <rodluger@gmail.com>
2018-03-13 14:36:06 -0700

    phase curves working

--- cpp/src/maps.h
+++ cpp/src/maps.h
@@ -123,25 +123,25 @@ namespace maps {
     // Compute the primitive integral helper matrix H
     template <typename T>
     T computeH(Greens<T>& G, int i, int j) {
-        return G.b; // debug
+        return 0; // TODO
     }
 
     // Compute the primitive integral helper matrix I
     template <typename T>
     T computeI(Greens<T>& G, int i, int j) {
-        return G.b; // debug
+        return 0; // TODO
     }
 
     // Compute the primitive integral helper matrix J
     template <typename T>
     T computeJ(Greens<T>& G, int i, int j) {
-        return G.b; // debug
+        return 0; // TODO
     }
 
     // Compute the primitive integral helper matrix M
     template <typename T>
     T computeM(Greens<T>& G, int i, int j) {
-        return G.b; // debug
+        return 0; // TODO
     }
 
     // Greens integration housekeeping data
@@ -149,6 +149,7 @@ namespace maps {
     class Greens {
 
         int lmax;
+        int N;
 
     public:
 
@@ -181,18 +182,23 @@ namespace maps {
         Primitive<T> J;
         Primitive<T> M;
 
+        Vector<T> sT;
+
         // Constructor
-        Greens(int lmax) : lmax(lmax), H(*this, lmax, computeH), I(*this, lmax, computeI), J(*this, lmax, computeJ), M(*this, lmax, computeM) {
+        Greens(int lmax) : lmax(lmax), H(*this, lmax, computeH),
+                           I(*this, lmax, computeI), J(*this, lmax, computeJ),
+                           M(*this, lmax, computeM) {
 
-            // DEBUG
-            b = 0.5;
-
-            std::cout << I.value(2, 1) << std::endl;
-
-
-            b = 0.75;
-
-            std::cout << M.value(2, 1) << std::endl;
+            // Initialize some stuff
+            N = 2 * lmax + 1;
+            if (N < 2) N = 2;
+            r.resize(N);
+            b_r.resize(N);
+            cosphi.resize(N);
+            sinphi.resize(N);
+            coslam.resize(N);
+            sinlam.resize(N);
+            sT.resize((lmax + 1) * (lmax + 1));
 
         }
 
@@ -206,6 +212,12 @@ namespace maps {
         int N;
         Vector<T> basis;
         bool needs_update;
+
+        // Temporary map vector
+        Vector<T> tmpvec;
+
+        // Private methods
+        void apply_rotation(UnitVector<T>& u, T costheta, T sintheta, Vector<T>& yin, Vector<T>& yout);
 
     public:
 
@@ -232,20 +244,27 @@ namespace maps {
             y = Vector<T>::Zero(N);
             p = Vector<T>::Zero(N);
             g = Vector<T>::Zero(N);
-            basis.resize(N, 1);
-            update(true);
-        }
-        // Constructor: initialize map to array
-        Map(Vector<T>& y) : y(y), lmax(floor(sqrt((double)y.size()) - 1)), R(lmax), C(lmax), G(lmax) {
-            N = (lmax + 1) * (lmax + 1);
+            tmpvec = Vector<T>::Zero(N);
             basis.resize(N, 1);
             update(true);
         }
 
-        // Declare our methods
-        T evaluate(const T& x, const T& y);
+        // Constructor: initialize map to array
+        Map(Vector<T>& y) : y(y), lmax(floor(sqrt((double)y.size()) - 1)), R(lmax), C(lmax), G(lmax) {
+            N = (lmax + 1) * (lmax + 1);
+            tmpvec = Vector<T>::Zero(N);
+            basis.resize(N, 1);
+            update(true);
+        }
+
+        // Public methods
+        T evaluate(const T& x0, const T& y0);
+        void rotate(UnitVector<T>& u, T theta, Vector<T>& yin, Vector<T>& yout);
+        void rotate(UnitVector<T>& u, T costheta, T sintheta, Vector<T>& yin, Vector<T>& yout);
         void rotate(UnitVector<T>& u, T theta);
+        void rotate(UnitVector<T>& u, T costheta, T sintheta);
         void update(bool force=false);
+        T flux(UnitVector<T>& u, T theta, T x0, T y0, T r);
         std::string repr(const double tol=1e-15);
 
     };
@@ -263,16 +282,16 @@ namespace maps {
 
     // Evaluate our map at a given (x, y) coordinate
     template <class T>
-    T Map<T>::evaluate(const T& x, const T& y) {
+    T Map<T>::evaluate(const T& x0, const T& y0) {
 
         // Update maps if necessary
         update();
 
         // Check if outside the sphere
-        if (x * x + y * y > 1.0) return NAN;
+        if (x0 * x0 + y0 * y0 > 1.0) return NAN;
 
         int l, m, mu, nu, n = 0;
-        T z = sqrt(1.0 - x * x - y * y);
+        T z0 = sqrt(1.0 - x0 * x0 - y0 * y0);
 
         // Compute the polynomial basis
         for (l=0; l<lmax+1; l++) {
@@ -280,9 +299,9 @@ namespace maps {
                 mu = l - m;
                 nu = l + m;
                 if ((nu % 2) == 0)
-                    basis(n) = pow(x, mu / 2) * pow(y, nu / 2);
+                    basis(n) = pow(x0, mu / 2) * pow(y0, nu / 2);
                 else
-                    basis(n) = pow(x, (mu - 1) / 2) * pow(y, (nu - 1) / 2) * z;
+                    basis(n) = pow(x0, (mu - 1) / 2) * pow(y0, (nu - 1) / 2) * z0;
                 n++;
             }
         }
@@ -292,22 +311,86 @@ namespace maps {
 
     }
 
-    // Rotate the map in-place
+    // Rotate a map `yin` and store the result in `yout`
     template <class T>
-    void Map<T>::rotate(UnitVector<T>& u, T theta) {
+    void Map<T>::apply_rotation(UnitVector<T>& u, T costheta, T sintheta, Vector<T>& yin, Vector<T>& yout) {
 
         // Compute the rotation matrix R
-        rotation::computeR(lmax, u, cos(theta), sin(theta), R.Complex, R.Real);
+        rotation::computeR(lmax, u, costheta, sintheta, R.Complex, R.Real);
 
         // Dot R in, order by order
         for (int l = 0; l < lmax + 1; l++) {
-            y.segment(l * l, 2 * l + 1) = R.Real[l] * y.segment(l * l, 2 * l + 1);
+            yout.segment(l * l, 2 * l + 1) = R.Real[l] * yin.segment(l * l, 2 * l + 1);
         }
 
-        // Flag for updating
-        needs_update = true;
-
         return;
+    }
+
+    // Shortcut to rotate the base map in-place given `theta`
+    template <class T>
+    void Map<T>::rotate(UnitVector<T>& u, T theta) {
+        apply_rotation(u, cos(theta), sin(theta), y, y);
+        needs_update = true;
+    }
+
+    // Shortcut to rotate the base map in-place given `costheta` and `sintheta`
+    template <class T>
+    void Map<T>::rotate(UnitVector<T>& u, T costheta, T sintheta) {
+        apply_rotation(u, costheta, sintheta, y, y);
+        needs_update = true;
+    }
+
+    // Shortcut to rotate an arbitrary map given `theta`
+    template <class T>
+    void Map<T>::rotate(UnitVector<T>& u, T theta, Vector<T>& yin, Vector<T>& yout) {
+        apply_rotation(u, cos(theta), sin(theta), yin, yout);
+    }
+
+    // Shortcut to rotate an arbitrary map given `costheta` and `sintheta`
+    template <class T>
+    void Map<T>::rotate(UnitVector<T>& u, T costheta, T sintheta, Vector<T>& yin, Vector<T>& yout) {
+        apply_rotation(u, costheta, sintheta, yin, yout);
+    }
+
+    // Compute the total flux during or outside of an occultation
+    template <class T>
+    T Map<T>::flux(UnitVector<T>& u, T theta, T x0, T y0, T r) {
+
+        // Pointer to the map we're integrating
+        // (defaults to the base map)
+        Vector<T>* ptry = &y;
+
+        // Impact parameter
+        T b = sqrt(x0 * x0 + y0 * y0);
+
+        // Check for complete occultation
+        if (b <= r - 1) return 0;
+
+        // Rotate the map into view if necessary and update our pointer
+        if (theta != 0) {
+            rotate(u, theta, (*ptry), tmpvec);
+            ptry = &tmpvec;
+        }
+
+        // No occultation: cake
+        if (b >= 1 + r) {
+            return C.rT * C.A1 * (*ptry);
+
+        // Occultation
+        } else {
+
+            // Align occultor with the +y axis if necessary
+            if (b > 0) {
+                rotate(u, y0 / b, x0 / b, (*ptry), tmpvec);
+                ptry = &tmpvec;
+            }
+
+            // Compute the sT vector (TODO)
+
+            // Dot the result in and we're done
+            return G.sT * C.A * (*ptry);
+
+        }
 
     }
 
