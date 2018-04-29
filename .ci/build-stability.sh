d2ed33fa4ddcbfa44862461e93801c7403787c0e
Rodrigo Luger <rodluger@gmail.com>
2018-04-29 17:03:40 -0300

    fix [stability]

--- .ci/build-stability.sh
+++ .ci/build-stability.sh
@@ -6,9 +6,11 @@ cd $TRAVIS_BUILD_DIR/tests
 python stability.py
 
 # Force push to the stability branch
+cd $TRAVIS_BUILD_DIR/tests
+git init
+git add -f *.pdf
+git -c user.name='travis' -c user.email='travis' commit -m "running stability tests"
+git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG HEAD:stability-pdf
+
+# Return to the top level
 cd $TRAVIS_BUILD_DIR
-git checkout --orphan stability-pdf
-git rm -rf . > /dev/null 2>&1
-git add -f tests/*.pdf
-git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
-git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG stability-pdf
