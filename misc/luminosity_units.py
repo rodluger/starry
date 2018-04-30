luminosity units fixed

misc/luminosity_units.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as pl
 import starry
 import numpy as np
 
+
 star = starry.Star()
 print(star.map)
 print(star.map.evaluate(), star.map.flux())
@@ -11,7 +12,7 @@ planet = starry.Planet(L=1e-2, r=0.1, a=3)
 sys = starry.System([star, planet])
 time = np.linspace(0.25, 1.25, 10000)
 sys.compute(time)
-pl.plot(time, sys.flux / np.nanmedian(sys.flux), label="Uniform")
+pl.plot(time, sys.flux, label="Uniform")
 
 star[1] = 0.4
 star[2] = 0.26
@@ -20,7 +21,7 @@ print(star.map)
 print(star.map.evaluate(), star.map.flux())
 
 sys.compute(time)
-pl.plot(time, sys.flux / np.nanmedian(sys.flux), label="Limb-Darkened")
+pl.plot(time, sys.flux, label="Limb-Darkened")
 
 pl.legend()
 pl.show()