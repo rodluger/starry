b3a5fed8dd28fcc9de559b42d44513cfbe6a09a1
Rodrigo Luger <rodluger@gmail.com>
2018-05-10 18:07:17 -0700

    Update build-docs.sh

--- .ci/build-docs.sh
+++ .ci/build-docs.sh
@@ -23,7 +23,7 @@ git add -f *.js
 git add -f _sources
 git add -f _static
 git add -f tutorials
-#git add -f _images
+git add -f _images
 git -c user.name='sphinx' -c user.email='sphinx' commit -m "rebuild gh-pages at ${rev}"
 git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG HEAD:gh-pages
 
