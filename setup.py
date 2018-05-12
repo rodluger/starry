7dcaadc4c3fc3a113ab6f17c72e997acfdab81a7
Rodrigo Luger <rodluger@gmail.com>
2018-05-12 11:40:25 -0700

    starry_maps tweak

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ setup(
     packages=['starry'],
     ext_modules=ext_modules,
     install_requires=['matplotlib',
-                      'starry_maps>=0.0.8',
+                      'starry_maps>=0.0.9',
                       'pybind11>=2.2'],
     cmdclass={'build_ext': BuildExt},
     zip_safe=False,
