fe0c9954b0252b9f1af94f711043463b5ad95e39
Rodrigo Luger <rodluger@gmail.com>
2018-05-23 11:41:43 -0700

    misc changes [skip ci]

--- misc/delay.py
+++ misc/delay.py
@@ -5,16 +5,16 @@ import starry
 
 star = starry.Star()
 planet = starry.Planet(L=1e-1, prot=1, porb=1, r=1)
-planet.map[1, 0] = 1 / np.sqrt(3)
+#planet.map[1, 0] = 1 / np.sqrt(3)
 sys = starry.System([star, planet])
 
-time = np.linspace(-0.1, 1.1, 10000)
+time = np.linspace(0.526, 0.5269, 10000)
 
-for R in [0, 1, 5, 10]:
+for R in [10]:
     sys.scale = R
     sys.compute(time)
     a = planet.a * R * 0.00464913034
-    pl.plot(time, sys.flux, label="a=%.1f AU" % a)
+    pl.plot(time, planet.flux, label="a=%.1f AU" % a)
 
 pl.legend()
 pl.show()
