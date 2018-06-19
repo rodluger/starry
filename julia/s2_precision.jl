a373649b520871a3d07c7052a913849cb61e0c90
Eric Agol <agol@uw.edu>
2018-06-19 14:24:05 -0700

    testing derivatives - found problem with Huv

--- julia/s2_precision.jl
+++ julia/s2_precision.jl
@@ -1,13 +1,19 @@
 using PyPlot
 include("/Users/ericagol/Students/Luger/Starry/starry/julia/s2_stable.jl")
 
-r = 100.
+r0 = [0.01,100.]
 nb = 1000
-b = [linspace(r-1+1e-8,r-1+1e-6,nb); linspace(r-1+1e-6,r-1+1e-3,nb); linspace(r-1+1e-3,r-1e-3,nb); linspace(r-1e-3,r-1e-6,nb); linspace(r-1e-6,r+1e-6,nb); linspace(r+1e-6,r+1e-3,nb);  linspace(r+1e-3,r+1-1e-3,nb); linspace(r+1-1e-3,r+1-1e-6,nb); linspace(r+1-1e-6,r+1-1e-8,nb)]
+fig,axes = subplots(2,2)
 
-s2_grid = s2.(r,b)
-s2_big = convert(Array{Float64,1},s2.(big(r),big.(b)))
-diff = s2_grid-s2_big
-plot(b,diff)
-xlabel("b")
-ylabel("s2-s2(big)")
+for i=1:2
+  r=r0[i]
+  b = [linspace(r-1+1e-8,r-1+1e-6,nb); linspace(r-1+1e-6,r-1+1e-3,nb); linspace(r-1+1e-3,r-1e-3,nb); linspace(r-1e-3,r-1e-6,nb); linspace(r-1e-6,r+1e-6,nb); linspace(r+1e-6,r+1e-3,nb);  linspace(r+1e-3,r+1-1e-3,nb); linspace(r+1-1e-3,r+1-1e-6,nb); linspace(r+1-1e-6,r+1-1e-8,nb)]
+
+  s2_grid = s2.(r,b)
+  s2_big = convert(Array{Float64,1},s2.(big(r),big.(b)))
+  diff = s2_grid-s2_big
+  ax[:plot](b,diff)
+  ax[:xlabel]("b")
+  ax[:ylabel]("s2-s2(big)")
+return
+end
