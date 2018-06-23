d017ee8a0b51277f7bd924dee6b7aa6f61beaf3d
Eric Agol <agol@uw.edu>
2018-06-22 18:12:43 -0700

    adding analytic derivatives of s_2

--- julia/test_s2_gradient.jl
+++ julia/test_s2_gradient.jl
@@ -65,33 +65,48 @@ println("gradient: ",s2_gradient," expected: ",-[2,-2./3.])
 
 # Try b=0 special case:
 r = 0.3; b= 0.0
+# Autodiff:
 s_2,s2_gradient= s2_grad(r,b)
-diff = s2_grad_num(r,b)-s2_gradient
-println("b : ",b," r: ",r," diff: ",diff)
-println("gradient: ",s2_gradient," expected: ",-pi*[2*r*sqrt(1.-r^2),0.])
+# Numerical:
+diff1 = s2_grad_num(r,b)-s2_gradient
+# Analytic:
+s2_grad_ana = zeros(2)
+s2!(r,b,s2_grad_ana)
+diff2 = s2_grad_ana-s2_gradient
+println("b : ",b," r: ",r," diff(num-auto): ",diff1," diff(ana-auto): ",diff2 )
+println("gradient: ",s2_grad_ana," expected: ",-pi*[2*r*sqrt(1.-r^2),0.])
 
 # Try r+b=1 special case:
 r = 0.2; b= 0.8
 s_2,s2_gradient= s2_grad(r,b)
 s2_grad_numeric = s2_grad_num(r,b)
-diff = s2_grad_numeric-s2_gradient
-println("b : ",b," r: ",r," diff: ",diff)
+diff1 = s2_grad_numeric-s2_gradient
+# Analytic:
+s2!(r,b,s2_grad_ana)
+diff2 = s2_grad_ana-s2_gradient
+println("b : ",b," r: ",r," diff(num-auto): ",diff1," diff(ana-auto): ",diff2 )
 println("gradient: ",s2_gradient," expected: ",-8r*sqrt(r*(1-r))*[1,-1/3])
 
 # Try r=b <1/2 special case:
 r = 0.3; b= 0.3
 s_2,s2_gradient= s2_grad(r,b)
 s2_grad_numeric = s2_grad_num(r,b)
-diff = s2_grad_numeric-s2_gradient
-println("b : ",b," r: ",r," diff: ",diff)
+diff1 = s2_grad_numeric-s2_gradient
+# Analytic:
+s2!(r,b,s2_grad_ana)
+diff2 = s2_grad_ana-s2_gradient
+println("b : ",b," r: ",r," diff(num-auto): ",diff1," diff(ana-auto): ",diff2 )
 println("gradient: ",s2_gradient," expected: ",-4*r*[cel_bulirsch(4*r^2,1.,1.,1-4*r^2),cel_bulirsch(4*r^2,1.,-1.,1.-4*r^2)/3.])
 
 # Try r=b > 1/2 special case:
 r = 3.0; b= 3.0
 s_2,s2_gradient= s2_grad(r,b)
 s2_grad_numeric = s2_grad_num(r,b)
-diff = s2_grad_numeric-s2_gradient
-println("b : ",b," r: ",r," diff: ",diff)
+diff1 = s2_grad_numeric-s2_gradient
+# Analytic:
+s2!(r,b,s2_grad_ana)
+diff2 = s2_grad_ana-s2_gradient
+println("b : ",b," r: ",r," diff(num-auto): ",diff1," diff(ana-auto): ",diff2 )
 println("gradient: ",s2_gradient," expected: ",-2*[cel_bulirsch(.25/r^2,1.,1.,0.),-cel_bulirsch(.25/r^2,1.,1.,2*(1.-.25/r^2))/3.])
 
 # Now, try a random case with b+r < 1:
@@ -101,9 +116,12 @@ while b+r > 1
 end
 s_2,s2_gradient= s2_grad(r,b)
 s2_grad_numeric = s2_grad_num(r,b)
-diff = s2_grad_numeric-s2_gradient
+diff1 = s2_grad_numeric-s2_gradient
 println("Test b+r < 1:")
-println("b : ",b," r: ",r," diff: ",diff)
+# Analytic:
+s2!(r,b,s2_grad_ana)
+diff2 = s2_grad_ana-s2_gradient
+println("b : ",b," r: ",r," diff(num-auto): ",diff1," diff(ana-auto): ",diff2 )
 
 # Now, try a random case with b+r > 1:
 b=r=0.
@@ -112,13 +130,16 @@ while b+r < 1
 end
 s_2,s2_gradient= s2_grad(r,b)
 s2_grad_numeric = s2_grad_num(r,b)
-diff = s2_grad_numeric-s2_gradient
+diff1 = s2_grad_numeric-s2_gradient
 println("Test b+r > 1:")
-println("b : ",b," r: ",r," diff: ",diff)
+# Analytic:
+s2!(r,b,s2_grad_ana)
+diff2 = s2_grad_ana-s2_gradient
+println("b : ",b," r: ",r," diff(num-auto): ",diff1," diff(ana-auto): ",diff2 )
 
 # Now, try out hard cases - ingress/egress of small planets:
 r0 = [0.01,100.0]
-nb = 20
+nb = 200
 #l_max = 20
 l_max = 10
 n_max = l_max^2+2*l_max
@@ -132,15 +153,13 @@ for i=1:2
   r=r0[i]
   if r < 1.0
     b = [linspace(1e-15,epsilon,nb); linspace(epsilon,delta,nb); linspace(delta,r-delta,nb);
-#     linspace(r-delta,r-epsilon,nb); linspace(r-epsilon,r,nb); linspace(r,r+epsilon,nb); linspace(r+epsilon,r+delta,nb);
-     linspace(r-delta,r-epsilon,nb); linspace(r-epsilon,r+epsilon,nb); linspace(r+epsilon,r+delta,nb);
-     linspace(r+delta,1-r-delta,nb); linspace(1-r-delta,1-r-epsilon,nb); linspace(1-r-epsilon,1-r+epsilon,nb);
-     linspace(1-r+epsilon,1-r+delta,nb); linspace(1-r+delta,1+r-delta,nb); linspace(1+r-delta,1+r-epsilon,nb);linspace(1+r-epsilon,1+r-1e-15,nb)]
+     r-logspace(log10(delta),log10(epsilon),nb); linspace(r-epsilon,r+epsilon,nb); r+logspace(log10(epsilon),log10(delta),nb);
+     linspace(r+delta,1-r-delta,nb); 1-r-logspace(log10(delta),log10(epsilon),nb); linspace(1-r-epsilon,1-r+epsilon,nb);
+     1-r+logspace(log10(epsilon),log10(delta),nb); linspace(1-r+delta,1+r-delta,nb); 1+r-logspace(log10(delta),log10(epsilon),nb);linspace(1+r-epsilon,1+r-1e-15,nb)]
   else
-    b = [linspace(r-1+1e-15,r-1+epsilon,nb); linspace(r-1+epsilon,r-1+delta,nb); linspace(r-1+delta,r-delta,nb);
-#     linspace(r-delta,r-epsilon,nb); linspace(r-epsilon,r,nb); linspace(r,r+epsilon,nb); linspace(r+epsilon,r+delta,nb);
-     linspace(r-delta,r-epsilon,nb); linspace(r-epsilon,r+epsilon,nb); linspace(r+epsilon,r+delta,nb);
-     linspace(r+delta,r+1-delta,nb); linspace(r+1-delta,r+1-epsilon,nb); linspace(r+1-epsilon,r+1-1e-15,nb)]
+    b = [linspace(r-1+1e-15,r-1+epsilon,nb); r-1+logspace(log10(epsilon),log10(delta),nb); linspace(r-1+delta,r-delta,nb);
+     r-logspace(log10(delta),log10(epsilon),nb); linspace(r-epsilon,r+epsilon,nb); r+logspace(log10(epsilon),log10(delta),nb);
+     linspace(r+delta,r+1-delta,nb); r+1-logspace(log10(delta),log10(epsilon),nb); linspace(r+1-epsilon,r+1-1e-15,nb)]
   end
   igrid=linspace(1,length(b),length(b))-1
   s2_jac_grid = zeros(length(b),2)
@@ -148,17 +167,18 @@ for i=1:2
   s2_jac_grid_num = zeros(length(b),2)
   for j=1:length(b)
 #    println("r: ",r," b: ",b[j])
-    s_2,s2_gradient= s2_grad(r,b[j])
+#    s_2,s2_gradient= s2_grad(r,b[j])
+    s_2= s2!(r,b[j],s2_grad_ana)
     s2_grid[j]=s_2
-    s2_jac_grid[j,:]=s2_gradient
+    s2_jac_grid[j,:]=s2_grad_ana
     s2_jac_grid_num[j,:]= s2_grad_num(r,b[j])
   println("r: ",r," b: ",b[j]," ds2/dr: ",s2_jac_grid[j,1]," ",s2_jac_grid[j,1]-s2_jac_grid_num[j,1])
   println("r: ",r," b: ",b[j]," ds2/db: ",s2_jac_grid[j,2]," ",s2_jac_grid[j,2]-s2_jac_grid_num[j,2])
   end
 # Now, make plots:
   ax = axes[i]
-  ax[:semilogy](b,abs.(s2_jac_grid[:,1]-s2_jac_grid_num[:,1]),lw=1,label="ds2/dr")
-  ax[:semilogy](b,abs.(s2_jac_grid[:,2]-s2_jac_grid_num[:,2]),lw=1,label="ds2/db")
+  ax[:semilogy](b,abs.(s2_jac_grid[:,1]-s2_jac_grid_num[:,1])+1e-18,lw=1,label="ds2/dr")
+  ax[:semilogy](b,abs.(s2_jac_grid[:,2]-s2_jac_grid_num[:,2])+1e-18,lw=1,label="ds2/db")
 #  ax[:semilogy](b,abs.(asinh.(s2_jac_grid[:,1])-asinh.(s2_jac_grid_num[:,1])),lw=1,label="ds2/dr")
 #  ax[:semilogy](b,abs.(asinh.(s2_jac_grid[:,2])-asinh.(s2_jac_grid_num[:,2])),lw=1,label="ds2/db")
   ax[:legend](loc="upper right",fontsize=6)
