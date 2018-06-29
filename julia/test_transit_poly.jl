bf0b1b9d06a0d56b62e8310c23a37bb41019c13f
Eric Agol <agol@uw.edu>
2018-06-29 14:54:47 -0700

    Implemented wrapper for u_n to call transit_poly_c with c_n

--- julia/test_transit_poly.jl
+++ julia/test_transit_poly.jl
@@ -1,24 +1,38 @@
 include("transit_poly.jl")
-nc = round(Int64,rand()*20)
-r=rand(); b=rand()*(1+r); c = rand(nc)
+nu = 2+ceil(Int64,rand()*20); r=rand(); b=rand()*(1+r); u = rand(nu); u *= rand()/sum(u)
+#r=rand(); b=rand()*(1+r); u = [0.,0.,0.,0.,1.0]; nu=length(u)
 
-flux = transit_poly(r,b,c)
+flux = transit_poly(r,b,u)
+@time flux = transit_poly(r,b,u)
 
 # Now, integrate by hand:
-nphi = 500; ns = 500; dphi = 2pi/nphi; ds=1/ns
-phi = linspace(.5*dphi,2pi-.5*dphi,nphi); s = linspace(.5*ds,1-.5*ds,ns)
-flux0 = zero(r); fobs = zero(r)
-for i=1:nphi, j=1:ns
-  x=s[j]*cos(phi[i]); y = s[j]*sin(phi[i])
+function transit_poly_int(r,b,u)
+s_1 = maximum([0.0,b-r]); s_2=minimum([1.0,b+r])
+ns = 10000; ds=(s_2-s_1)/ns
+s = linspace(s_1+.5*ds,s_2-.5*ds,ns)
+fobs = zero(r)
+for j=1:ns
+  if s[j] < r-b
+    dphi = 2pi
+  else
+    dphi = 2*acos((s[j]^2+b^2-r^2)/(2*b*s[j]))
+  end
   z = sqrt(1-s[j]^2)
-  imu = c[1]+c[2]*z
-  for k=2:length(c)-1
-    imu += c[k+1]*(z^k*(k+2)-k*z^(k-2))
+  imu = 1.0
+  for n=1:nu
+    imu -= u[n]*(1-z)^n
   end
-  flux0 += s[j]*ds*dphi*imu
-  if (y-b)^2+x^2 > r^2
-    fobs += s[j]*ds*dphi*imu
-  end
+  fobs += s[j]*ds*dphi*imu
 end
-#println("r: ",r," b: ",b,"f_an: ",flux," flux: ",flux0," f_num: ",fobs/flux0," f_exp: ",pi*(c[1]+2/3*c[2]))
-println("r: ",r," b: ",b," f_an: ",flux," f_num: ",fobs/flux0)
+norm = 1.
+for n=1:nu
+  norm -= 2*u[n]/(2+3*n+n^2)
+end
+fobs /= pi*norm
+fobs = 1-fobs
+return fobs
+end
+
+f_num = transit_poly_int(r,b,u)
+@time f_num = transit_poly_int(r,b,u)
+println("r: ",r," b: ",b," f_an: ",flux," f_num: ",f_num)
--- julia/transit_poly.jl
+++ julia/transit_poly.jl
@@ -7,12 +7,38 @@
 
 include("sn_bigr.jl")
 
-function transit_poly(r::T,b::T,c_n::Array{T,1}) where {T <: Real}
+function transit_poly(r::T,b::T,u_n::Array{T,1}) where {T <: Real}
+# Transform the u_n coefficients to c_n, which are coefficients
+# of the basis in which the P(G_n) functions are computed.
+n = length(u_n)
+c_n = zeros(typeof(r),n+3)
+a_n = zeros(typeof(r),n+1)
+a_n[1] = one(r)  # Add in the first constant coefficient term
+for i=1:n
+  # Compute the contribution to a_n*\mu^n
+  for j=0:i
+    a_n[j+1] -= u_n[i]*binomial(i,j)*(-1)^j
+#    println("i: ",i," j: ",j," a_i: ",a_n[j+1])
+  end
+end
+# Now, compute the c_n coefficients:
+for j=n:-1:2
+  c_n[j+1] = a_n[j+1]/(j+2)+c_n[j+3]
+end
+c_n[2] = a_n[2]+3*c_n[4]
+c_n[1] = a_n[1]+2*c_n[3]
+#println("u_n: ",u_n)
+#println("a_n: ",a_n)
+#println("c_n: ",c_n)
+return transit_poly_c(r,b,c_n[1:n+1])
+end
+
+function transit_poly_c(r::T,b::T,c_n::Array{T,1}) where {T <: Real}
 # Number of limb-darkening components to include (beyond 0 and 1):
 N_c = length(c_n)-1
 # We are parameterizing these with the function:
 # g_n = c_n [(n+2) z^n - n z^{n-2}] for n >= 2
-# while g_{-2} = c_0 z^0 (uniform source) and g_{-1} = c_1 z^1 (linear limb-darkening)
+# while g_{0} = c_0 z^0 (uniform source) and g_{1} = c_1 z^1 (linear limb-darkening)
 # which gives a Green's integral of:
 # P(G_n) = \int_{\pi-\phi}^{2\pi+\phi} (1-r^2-b^2-2br s_\varphi)^{n/2} (r+b s_\varphi) d\varphi
 # for which we have a solution in terms of I_v (for even n) and J_v (for odd n).
@@ -32,10 +58,18 @@ if r >= 1+b
 end
 if b == 0.0
   # Annular eclipse - integrate around the full boundary of both bodies:
-  k2 = Inf; kc = 0.0
+  flux = zero(r); sqrt1mr2 = sqrt(1-r^2)
+  flux = (c_n[1]*(1-r^2)+2/3*c_n[2]*sqrt1mr2^3)
+  fac= 2r^2*(1-r^2)
+  for i=2:N_c
+    flux += -c_n[i+1]*fac
+    fac *= sqrt1mr2
+  end
+  return flux/(c_n[1]+2*c_n[2]/3)
 else
 # Next, compute k^2 = m:
-  k2 = (1.0-(b-r)^2)/(4b*r);
+  onembmr2=(r-b+1)*(b-r+1); fourbr = 4b*r
+  k2 = onembmr2/fourbr
   if k2 > 1
     if k2 > 2.0
       kc = sqrt(1.-inv(k2))
@@ -78,35 +112,31 @@ for n=2:N_c
   if iseven(n)
 # For even values of n, sum over I_v:
     n0 = convert(Int64,n/2)
-    coeff = (-1)^n0
+    coeff = (-fourbr)^n0
     # Compute i=0 term
     pofgn = coeff*((r-b)*Iv[n0+1]+2b*Iv[n0+2])
-    kn = one(r)
 # For even n, compute coefficients for the sum over I_v:
 #    println("n0: ",n0," i: ",0," coeff: ",coeff)
     for i=1:n0
-      coeff *= -(n0-i+1)/i
+      coeff *= -(n0-i+1)/i*k2
 #      println("n0: ",n0," i: ",i," coeff: ",coeff)
-      kn *= k2
-      pofgn += coeff*kn*((r-b)*Iv[n0-i+1]+2b*Iv[n0-i+2])
+      pofgn += coeff*((r-b)*Iv[n0-i+1]+2b*Iv[n0-i+2])
     end
-    pofgn *= 2r*(4*b*r)^n0
+    pofgn *= 2r
   else
 # Now do the same for odd N_c in sum over J_v:
     n0 = convert(Int64,(n-3)/2)
-    coeff = (-1)^n0
+    coeff = (-fourbr)^n0
     # Compute i=0 term
     pofgn = coeff*((r-b)*Jv[n0+1]+2b*Jv[n0+2])
-    kn = one(r)
 #    println("n0: ",n0," i: ",0," coeff: ",coeff)
 # For even n, compute coefficients for the sum over I_v:
     for i=1:n0
-      coeff *= -(n0-i+1)/i
+      coeff *= -(n0-i+1)/i*k2
 #      println("n0: ",n0," i: ",i," coeff: ",coeff)
-      kn *= k2
-      pofgn += coeff*kn*((r-b)*Jv[n0-i+1]+2b*Jv[n0-i+2])
+      pofgn += coeff*((r-b)*Jv[n0-i+1]+2b*Jv[n0-i+2])
     end
-    pofgn *= 2r*(1-(b-r)^2)^1.5*(4*b*r)^n0
+    pofgn *= 2r*onembmr2^1.5
   end
 #  pofgn_num = sum(sqrt.(1-r^2-b^2-2*b*r*sin.(phigrid)).^n.*(r+b.*sin.(phigrid))*r*dphi)
 #  println("n: ",n," P(G_n): ",pofgn," P(G_n),num: ",pofgn_num)
