7b49655755b19762960460d4b94edd0f4bb24ab7
Rodrigo Luger <rodluger@gmail.com>
2018-08-08 08:36:14 -0700

    re-added macros

--- starry2/utils.h
+++ starry2/utils.h
@@ -14,6 +14,50 @@ Miscellaneous stuff used throughout the code.
 #include <limits>
 #include "errors.h"
 
+//! Number of digits for the multiprecision type in starry.multi
+#ifndef STARRY_NMULTI
+#define STARRY_NMULTI                           32
+#endif
+
+//! Max iterations in elliptic integrals
+#ifndef STARRY_ELLIP_MAX_ITER
+#define STARRY_ELLIP_MAX_ITER                   200
+#endif
+
+//! Max iterations in computation of I_v and J_v
+#ifndef STARRY_IJ_MAX_ITER
+#define STARRY_IJ_MAX_ITER                      200
+#endif
+
+//! Max iterations in Kepler solver
+#ifndef STARRY_KEPLER_MAX_ITER
+#define STARRY_KEPLER_MAX_ITER                  100
+#endif
+
+//! Re-parameterize solution vector when
+//! abs(b - r) < STARRY_EPS_BMR_ZERO
+#ifndef STARRY_EPS_BMR_ZERO
+#define STARRY_EPS_BMR_ZERO                     1e-2
+#endif
+
+//! Re-parameterize solution vector when
+//! 1 - STARRY_EPS_BMR_ONE < abs(b - r) < 1 + STARRY_EPS_BMR_ONE
+#ifndef STARRY_EPS_BMR_ONE
+#define STARRY_EPS_BMR_ONE                      1e-5
+#endif
+
+//! Re-parameterize solution vector when
+//! 1 - STARRY_EPS_BMR_ONE < abs(b + r) < 1 + STARRY_EPS_BPR_ONE
+#ifndef STARRY_EPS_BPR_ONE
+#define STARRY_EPS_BPR_ONE                      1e-5
+#endif
+
+//! Re-parameterize solution vector when
+//! abs(b) < STARRY_EPS_B_ZERO
+#ifndef STARRY_EPS_B_ZERO
+#define STARRY_EPS_B_ZERO                       1e-1
+#endif
+
 namespace utils {
 
     //! Multiprecision datatype backend
