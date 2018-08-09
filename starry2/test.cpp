a7ae38e40a2fbad7b00fc043719da8720f68c841
Rodrigo Luger <rodluger@gmail.com>
2018-08-09 09:50:41 -0700

    tweak test

--- starry2/test.cpp
+++ starry2/test.cpp
@@ -2,24 +2,28 @@
 #include <iostream>
 #include "maps.h"
 #include "utils.h"
+using namespace utils;
 
 int main() {
 
-    int lmax = 1;
-    int nwav = 2;
+    int lmax = 2;
+    int nwav = 1;
     maps::Map<double> map(lmax, nwav);
 
-    // First wavelength is Y_{1,0}
-    // Second wavelength is Y_{1,1}
-    utils::VectorT<double> coeff(nwav);
-    coeff << 1, 0;
-    map.setYlm(1, 0, coeff);
-    coeff << 0, 1;
-    map.setYlm(1, 1, coeff);
+    for (int l = 0; l < lmax + 1; ++l) {
+        for (int m = -l; m < l + 1; ++m) {
+            map.setYlm(l, m, Vector<double>::Constant(1, 1));
+        }
+    }
 
+    int nt = 100000;
+    Vector<double> theta = Vector<double>::LinSpaced(nt, 0, 45);
+    Vector<double> xo = Vector<double>::LinSpaced(nt, -1.2, 1.15);
+    Vector<double> yo = Vector<double>::LinSpaced(nt, -0.2, 0.33);
+    double ro = 0.1;
 
-    std::cout << map.flux(0.001, 1.3, 0.5, 0., true).transpose() << std::endl;
-
-    std::cout << map.dF << std::endl;
+    for (size_t t = 0; t < nt; ++t) {
+        map.flux(0, xo(t), yo(t), ro, false);
+    }
 
 }
