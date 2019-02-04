d3c8866d0e10bc0384653b2ad469034258dc9935
Rodrigo Luger <rodluger@gmail.com>
2019-02-04 10:21:44 -0500

    ld stability done

--- tests/test_ld_stability.py
+++ tests/test_ld_stability.py
@@ -12,67 +12,95 @@ map_multi[0, 0] = 1
 map_multi[:] = 1
 
 
-def test_b_near_zero(tol=1e-12):
+def test_b_near_zero(ftol=1e-11, gtol=1e-11):
     b = 10.0 ** np.linspace(-18, -6, 100)
     b[0] = 0
     f = map.flux(xo=b, yo=0.0, ro=0.1)
-    assert(np.std(f) < tol)
+    f_multi = map_multi.flux(xo=b, yo=0.0, ro=0.1)
+    assert(np.max(np.abs(f - f_multi)) < ftol)
+    _, g = map.flux(xo=b, yo=0.0, ro=0.1, gradient=True)
+    _, g_multi = map_multi.flux(xo=b, yo=0.0, ro=0.1, gradient=True)
+    for key in g.keys():
+        assert(np.max(np.abs(g[key] - g_multi[key])) < gtol)
 
 
-def test_b_near_one_plus_r(tol=1e-11):
+def test_b_near_one_plus_r(ftol=1e-11, gtol=1e-11):
     ro = 0.1
     b = 1 + ro - 10.0 ** np.linspace(-18, -6, 100)
     b[0] = 1 + ro
     f = map.flux(xo=b, yo=0.0, ro=ro)
     f_multi = map_multi.flux(xo=b, yo=0.0, ro=ro)
-    assert(np.max(np.abs(f - f_multi)) < tol)
+    assert(np.max(np.abs(f - f_multi)) < ftol)
+    _, g = map.flux(xo=b, yo=0.0, ro=ro, gradient=True)
+    _, g_multi = map_multi.flux(xo=b, yo=0.0, ro=ro, gradient=True)
+    for key in g.keys():
+        assert(np.max(np.abs(g[key] - g_multi[key])) < gtol)
 
 
-def test_b_near_one_minus_r(tol=1e-11):
+def test_b_near_one_minus_r(ftol=1e-11, gtol=1e-11):
     ro = 0.1
     b = 1 - ro - 10.0 ** np.linspace(-18, -6, 100)
     b[0] = 1 - ro
     f = map.flux(xo=b, yo=0.0, ro=ro)
     f_multi = map_multi.flux(xo=b, yo=0.0, ro=ro)
-    assert(np.max(np.abs(f - f_multi)) < tol)
+    assert(np.max(np.abs(f - f_multi)) < ftol)
+    _, g = map.flux(xo=b, yo=0.0, ro=ro, gradient=True)
+    _, g_multi = map_multi.flux(xo=b, yo=0.0, ro=ro, gradient=True)
+    for key in g.keys():
+        assert(np.max(np.abs(g[key] - g_multi[key])) < gtol)
 
 
-def test_b_equals_r_equals_half(tol=1e-11):
+def test_b_equals_r_equals_half(ftol=1e-11, gtol=1e-5):
+    # TODO: The gradient precision is pretty 
+    # poor in this case. This is a *crazy* unlikely
+    # edge case, though.
     ro = 0.5
     b = 0.5 - 10.0 ** np.linspace(-18, -6, 100)
     b[0] = 0.5
     f = map.flux(xo=b, yo=0.0, ro=ro)
     f_multi = map_multi.flux(xo=b, yo=0.0, ro=ro)
-    assert(np.max(np.abs(f - f_multi)) < tol)
+    assert(np.max(np.abs(f - f_multi)) < ftol)
+    _, g = map.flux(xo=b, yo=0.0, ro=ro, gradient=True)
+    _, g_multi = map_multi.flux(xo=b, yo=0.0, ro=ro, gradient=True)
+    for key in g.keys():
+        assert(np.max(np.abs(g[key] - g_multi[key])) < gtol)
 
 
-def test_r_near_one_b_is_zero(tol=1e-12):
+def test_r_near_one_b_is_zero(ftol=1e-12, gtol=1e-11):
     ro = 0.5 - 10.0 ** np.linspace(-18, -6, 100)
     ro[0] = 0.5
     b = 0.0
     f = map.flux(xo=b, yo=0.0, ro=ro)
     f_multi = map_multi.flux(xo=b, yo=0.0, ro=ro)
-    assert(np.max(np.abs(f - f_multi)) < tol)
+    assert(np.max(np.abs(f - f_multi)) < ftol)
+    _, g = map.flux(xo=b, yo=0.0, ro=ro, gradient=True)
+    _, g_multi = map_multi.flux(xo=b, yo=0.0, ro=ro, gradient=True)
+    for key in g.keys():
+        assert(np.max(np.abs(g[key] - g_multi[key])) < gtol)
 
 
-def test_r_large(tol=1e-6):
+def test_r_large(ftol=1e-6, gtol=1e-5):
     # TODO: For r = 100, we currently only get ~ppm precision
-    # on the flux for l = 10. This is kind of a bummer -- 
-    # let's think of ways to improve this.
+    # on the flux and its gradient for l = 10. This is kind of a 
+    # bummer -- let's think of ways to improve this.
     ro = 100
     b = np.linspace(ro - 1, ro + 1, 500)
     f = map.flux(xo=b, yo=0.0, ro=ro)
     f_multi = map_multi.flux(xo=b, yo=0.0, ro=ro)
-    assert(np.max(np.abs(f - f_multi)) < tol)
+    assert(np.max(np.abs(f - f_multi)) < ftol)
+    _, g = map.flux(xo=b, yo=0.0, ro=ro, gradient=True)
+    _, g_multi = map_multi.flux(xo=b, yo=0.0, ro=ro, gradient=True)
+    for key in g.keys():
+        assert(np.max(np.abs(g[key] - g_multi[key])) < gtol)
 
 
-def test_r_small(tol=1e-11):
+def test_r_small(ftol=1e-11, gtol=1e-11):
     b = 0.5
     ro = 10 ** np.linspace(-18, -1, 500)
     f = map.flux(xo=b, yo=0.0, ro=ro)
     f_multi = map_multi.flux(xo=b, yo=0.0, ro=ro)
-    assert(np.max(np.abs(f - f_multi)) < tol)
-
-
-if __name__ == "__main__":
-    test_r_small()
+    assert(np.max(np.abs(f - f_multi)) < ftol)
+    _, g = map.flux(xo=b, yo=0.0, ro=ro, gradient=True)
+    _, g_multi = map_multi.flux(xo=b, yo=0.0, ro=ro, gradient=True)
+    for key in g.keys():
+        assert(np.max(np.abs(g[key] - g_multi[key])) < gtol)
