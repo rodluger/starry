6ade8ce198e9837b02f462070469b08e709b1b85
Rodrigo Luger <rodluger@gmail.com>
2019-04-01 10:58:42 -0400

    debug

--- tests/test_refl_phase_curves.py
+++ tests/test_refl_phase_curves.py
@@ -41,3 +41,28 @@ class TestReflectedPhaseCurves:
 
         # Compare
         assert np.allclose(flux_num, flux, atol=1e-2, rtol=1e-2)
+
+
+if __name__ == "__main__":
+    ydeg, udeg = 5, 2
+    map = starry.Map(ydeg=ydeg, udeg=udeg, reflected=True)
+    map[1:, :] = 0.05 * np.random.randn(map.Ny - 1)
+    if udeg > 0:
+        map[1:] = 0.5
+
+    # Rotate the map and the source over time
+    source = np.array([[np.sin(3 * t), np.cos(4 * t), np.sin(t)] 
+                    for t in np.linspace(0, 5, 100)])
+    theta = np.linspace(0, 180, 100)
+
+    # Compute the flux analytically
+    flux = map.flux(theta=theta, source=source)
+    flux /= np.nanmedian(flux)
+
+    # Compute it numerically
+    flux_num = np.nansum(map.render(res=50, theta=theta, 
+                            source=source), axis=(1, 2))
+    flux_num /= np.nanmedian(flux_num)
+
+    # Compare
+    assert np.allclose(flux_num, flux, atol=1e-2, rtol=1e-2)
