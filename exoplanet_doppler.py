8f773c20b5e2746a0fa524ca1446d1c9bd3b003f
Rodrigo Luger <rodluger@gmail.com>
2019-04-17 09:00:17 -0300

    woot; optim working

--- exoplanet_doppler.py
+++ exoplanet_doppler.py
@@ -38,40 +38,28 @@ x = coords[0].eval()
 y = coords[1].eval()
 z = -coords[2].eval()
 truths["rv"] = map.rv(xo=x, yo=y, zo=z, ro=truths["r"])
-
-# Noise it
 rv = truths["rv"] + truths["rv_err"] * np.random.randn(len(t))
 
-# Plot it
-plt.plot(t, rv, 'k.', alpha=0.3, ms=3)
-plt.plot(t, truths["rv"])
-plt.show()
-
-# Sample it
-sampler = xo.PyMC3Sampler(window=100, finish=200)
-
+# Define our model
 starry_op = starry.ops.DopplerMapOp(udeg=udeg)
-
 with pm.Model() as model:
 
+    # Wide priors
     inc = pm.Uniform("inc", 0, 180)
     obl = pm.Uniform("obl", -180, 180)
     alpha = pm.Uniform("alpha", 0, 1)
     veq = pm.Uniform("veq", 0, 10)
-    period = pm.Uniform("period", 1.0, 100.0)
-    t0 = pm.Uniform("t0", -1, 1)
+    period = pm.Uniform("period", 1.0, 100.0, testval=5.0)
+    t0 = pm.Uniform("t0", -0.1, 0.1)
     u = xo.distributions.QuadLimbDark("u", testval=np.array([0.3, 0.2]))
     r = pm.Uniform("r", 0.01, 0.25)
-    b = pm.Uniform("b", 0, 1.25)
+    b = pm.Uniform("b", -1.25, 1.25)
 
     # We're not fitting for theta
-    # @dfm: How do I prevent pymc3 from fitting for it?
-    theta = np.ones_like(t)  # np.ones_like(t) * pm.Uniform("theta", 0, 1)
+    theta = np.zeros_like(t)
     
     # The map Ylm degree is zero, so there are no Ylms to fit
     y = np.empty(0)
-    # y = tt.as_tensor_variable([], name='y')
-    # y.name = 'y'
 
     # Vectorize the occultor radius
     rs = np.ones_like(t) * r
@@ -102,8 +90,25 @@ with pm.Model() as model:
     # Here we track the value of the model light curve for plotting later
     pm.Deterministic("rv_model", rv_model)
 
+    # Save our initial guess
+    model_guess = xo.eval_in_model(rv_model)
+
     # The likelihood function assuming known Gaussian uncertainty
     pm.Normal("obs", mu=rv_model, sd=truths["rv_err"], observed=rv)
 
     # Fit for the maximum a posteriori parameters
     map_soln = xo.optimize(start=model.test_point)
+
+
+# Plot it
+fig, ax = plt.subplots(1, figsize=(8, 4))
+ax.plot(t, rv, 'k.', alpha=0.3, ms=3)
+ax.set_ylim(*ax.get_ylim())
+ax.plot(t, truths["rv"], label="True")
+ax.plot(t, model_guess, label="Initial")
+ax.plot(t, map_soln["rv_model"], label="Optimized")
+ax.legend()
+plt.show()
+
+# Sample it
+#sampler = xo.PyMC3Sampler(window=100, finish=200)
