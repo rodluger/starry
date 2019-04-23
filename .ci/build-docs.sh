bc645955de59552ec98705bb975b4f0d5e3347f7
Rodrigo Luger <rodluger@gmail.com>
2019-04-23 17:06:25 -0300

    Attempt at docs versioning

--- .ci/build-docs.sh
+++ .ci/build-docs.sh
@@ -12,22 +12,29 @@ echo "Building docs from ${branch} branch..."
 # Get git hash
 rev=$(git rev-parse --short HEAD)
 
-# Initialize a new git repo in the build dir,
-# add the necessary files, and force-push
-# to the gh-pages branch
-cd $TRAVIS_BUILD_DIR/docs/_build/html
+# Get the current starry version
+STARRY_VERSION=$(python -c "import starry; print('v' + starry.__version__)")
+
+# Go to the html build
+cd $TRAVIS_BUILD_DIR/docs/_build/
+
+# Clone the `gh-pages` branch
+mkdir gh-pages
+cd gh-pages
+git clone -b gh-pages --single-branch https://github.com/rodluger/starry.git .
+
+# Reset git tracking & update the current version's docs
+rm -rf .git
+mv ../html $STARRY_VERSION
+
+# Commit & force push back
 git init
 touch .nojekyll
 git add -f .nojekyll
-git add -f *.html
-git add -f *.js
-git add -f _sources
-git add -f _static
-git add -f tutorials
-git add -f proofs
-git add -f _images
-git -c user.name='sphinx' -c user.email='sphinx' commit -m "rebuild gh-pages at ${rev}"
+git add -f *
+git -c user.name='sphinx' -c user.email='sphinx' commit -m "rebuild gh-pages at ${rev} for ${STARRY_VERSION}"
 git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG HEAD:gh-pages
 
 # Return to the top level
 cd $TRAVIS_BUILD_DIR
+
