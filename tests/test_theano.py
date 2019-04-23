369a0cbd965e257165d46f335870ec43dcd5069d
Rodrigo Luger <rodluger@gmail.com>
2019-04-23 15:03:14 -0300

    updated theano test

--- tests/test_theano.py
+++ tests/test_theano.py
@@ -14,54 +14,6 @@ def test_doppler():
         "inc":      75.0,
         "obl":      30.0,
         "alpha":    0.40,
-        "veq":      3.00,
-        "xo":       0.15,
-        "yo":       0.2,
-        "zo":       1.0,
-        "ro":       0.1,
-        "u":        [0.4, 0.26]
-    }
-    theano_kwargs = {}
-    for key in kwargs.keys():
-        theano_kwargs[key] = tt.as_tensor_variable(np.float64(kwargs[key]), name=key)
-
-    # Compute the rv and its gradient using starry
-    map = DopplerMap(ydeg=1, udeg=2) 
-    map.inc = kwargs.pop("inc")
-    map.obl = kwargs.pop("obl")
-    map.alpha = kwargs.pop("alpha")
-    map.veq = kwargs.pop("veq")
-    map[1:, :] = kwargs.pop("y")
-    map[1:] = kwargs.pop("u")
-    rv, grad = map.rv(gradient=True, **kwargs)
-
-    # Instantiate the theano op
-    model = map.rv_op(**theano_kwargs)
-
-    # Compare
-    for key in theano_kwargs.keys():
-        if key == "zo":
-            continue
-        assert np.allclose(
-            np.squeeze(grad[key]),
-            np.squeeze(theano.grad(model[0], theano_kwargs[key]).eval())
-        ), key
-
-
-def test_doppler_no_longer_broken():
-    """
-    If we give two variables the same value, theano *sums* over their
-    gradients, and yields the *same value* for the gradient with respect
-    to both one of them. Why is that??
-
-    """
-    # Define all arguments
-    kwargs = {
-        "y":        [0.25, 0.25, 0.25],
-        "theta":    30.0,
-        "inc":      75.0,
-        "obl":      30.0,
-        "alpha":    0.40,
         "veq":      0.40,
         "xo":       0.15,
         "yo":       0.2,
@@ -71,6 +23,10 @@ def test_doppler_no_longer_broken():
     }
     theano_kwargs = {}
     for key in kwargs.keys():
+        # NOTE: Use `theano.shared` instead of `as_tensor_variable` 
+        # to prevent theano from treating variables whose values
+        # are the same as the same variable! See
+        # https://github.com/rodluger/starry/pull/195
         theano_kwargs[key] = theano.shared(np.float64(kwargs[key]), name=key)
 
     # Compute the rv and its gradient using starry
