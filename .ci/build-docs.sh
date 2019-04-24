023abe29cc4983080f55968ff8958ae71d6f7f18
Rodrigo Luger <rodluger@gmail.com>
2019-04-24 13:21:25 -0300

    security [skip ci]

--- .ci/build-docs.sh
+++ .ci/build-docs.sh
@@ -34,7 +34,7 @@ touch .nojekyll
 git add -f .nojekyll
 git add -f *
 git -c user.name='sphinx' -c user.email='sphinx' commit -m "rebuild gh-pages at ${rev} for ${STARRY_VERSION}"
-git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG HEAD:gh-pages
+git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG HEAD:gh-pages >/dev/null 2>&1
 
 # Return to the top level
 cd $TRAVIS_BUILD_DIR
