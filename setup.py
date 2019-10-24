e9274668b52b020e933b8cacb3feeeebc6ad4c54
Dan F-M <foreman.mackey@gmail.com>
2019-10-24 09:56:27 -0400

    fixing windows build

--- setup.py
+++ setup.py
@@ -90,7 +90,20 @@ def has_flag(compiler, flagname):
     Return a boolean indicating whether a flag name is supported on
     the specified compiler.
     """
+    import shutil
     import tempfile
+
+    if compiler.compiler_type == "msvc":
+        fn = ".starry-compile-temp.cpp"
+        with open(fn, "w") as f:
+            f.write("int main (int argc, char **argv) { return 0; }")
+        try:
+            compiler.compile([fn], extra_postargs=[flagname])
+        except setuptools.distutils.errors.CompileError:
+            shutil.rmtree(fn)
+            return False
+        shutil.rmtree(fn)
+        return True
 
     with tempfile.NamedTemporaryFile("w", suffix=".cpp", dir=".") as f:
         f.write("int main (int argc, char **argv) { return 0; }")
