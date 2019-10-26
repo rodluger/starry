520d97bfef7795494a1cd8858fa944cd71640618
Rodrigo Luger <rodluger@gmail.com>
2019-10-26 11:47:29 -0400

    experimenting with test publishing

---
+++ starry/extensions/tests/greedy/test_greedy_example.py
@@ -0,0 +1,13 @@
+import starry
+
+starry.config.lazy = False
+
+
+def test_greedy_example():
+    # Test greedy extensions here
+    assert True
+
+
+def test_greedy_example_failure():
+    # Let's see what happens when an extension test fails
+    assert False
