bugfix in healpix map computation

starry/maps.py
@@ -578,9 +578,9 @@ class MapBase(object):
         if not self.__props__["limbdarkened"]:
             kwargs.pop("projection", None)
         if self.__props__["reflected"]:
-            kwargs.pop("xo", None)
-            kwargs.pop("yo", None)
-            kwargs.pop("zo", None)
+            kwargs.pop("xs", None)
+            kwargs.pop("ys", None)
+            kwargs.pop("zs", None)
         self._check_kwargs("show", kwargs)
 
     def limbdark_is_physical(self):
@@ -889,7 +889,8 @@ class YlmBase(object):
         self,
         image,
         healpix=False,
-        sampling_factor=8,
+        nside=None,
+        max_iter=3,
         sigma=None,
         force_psd=False,
         **kwargs
@@ -905,10 +906,6 @@ class YlmBase(object):
                 array, or a ``healpix`` map array (if ``healpix`` is True).
             healpix (bool, optional): Treat ``image`` as a ``healpix`` array?
                 Default is False.
-            sampling_factor (int, optional): Oversampling factor when computing
-                the ``healpix`` representation of an input image or array.
-                Default is 8. Increasing this number may improve the fidelity
-                of the expanded map, but the calculation will take longer.
             sigma (float, optional): If not None, apply gaussian smoothing
                 with standard deviation ``sigma`` to smooth over
                 spurious ringing features. Smoothing is performed with
@@ -916,11 +913,26 @@ class YlmBase(object):
                 Default is None.
             force_psd (bool, optional): Force the map to be positive
                 semi-definite? Default is False.
+            nside (int, optional): The ``NSIDE`` argument to a Healpix
+                map. This controls the angular resolution of the image; increase
+                this for maps with higher fidelity, at the expense of extra
+                compute time. Default is the degree of the map, rounded
+                up to the closest power of 2.
+            max_iter (int, optional): Maximum number of iterations in trying
+                to convert the map to a Healpix array. Each iteration, the
+                input array is successively doubled in size in order to
+                increase the angular coverage of the input. Default is 3. If
+                the number of iterations exceeds this value, an error will
+                be raised.
             kwargs (optional): Any other kwargs passed directly to
                 :py:meth:`minimize` (only if ``psd`` is True).
         """
         # Not implemented for spectral
         self._no_spectral()
+
+        # Default number of sides
+        if nside is None:
+            nside = int(2 ** np.ceil(np.log2(self.ydeg)))
 
         # Is this a file name?
         if type(image) is str:
@@ -928,7 +940,8 @@ class YlmBase(object):
                 image,
                 lmax=self.ydeg,
                 sigma=sigma,
-                sampling_factor=sampling_factor,
+                nside=nside,
+                max_iter=max_iter,
             )
         # or is it an array?
         elif type(image) is np.ndarray:
@@ -937,14 +950,16 @@ class YlmBase(object):
                     image,
                     lmax=self.ydeg,
                     sigma=sigma,
-                    sampling_factor=sampling_factor,
+                    nside=nside,
+                    max_iter=max_iter,
                 )
             else:
                 y = array2map(
                     image,
                     lmax=self.ydeg,
                     sigma=sigma,
-                    sampling_factor=sampling_factor,
+                    nside=nside,
+                    max_iter=max_iter,
                 )
         else:
             raise ValueError("Invalid `image` value.")

starry/sht.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 """Healpy-based spherical harmonic transform utilities for starry."""
 import numpy as np
+from PIL import Image
+from matplotlib.image import pil_to_array
+import os
+from scipy import ndimage
 
 try:
     import healpy as hp
 except ImportError:
     hp = None
-from PIL import Image
-from matplotlib.image import pil_to_array
-import os
 
-
-__all__ = ["image2map", "healpix2map", "array2map"]
+__all__ = ["image2map", "healpix2map", "array2map", "array2healpix"]
 
 
 def healpix2map(healpix_map, lmax=10, **kwargs):
@@ -50,7 +50,7 @@ def healpix2map(healpix_map, lmax=10, **kwargs):
     return ylm
 
 
-def image2map(image, lmax=10, **kwargs):
+def image2map(image, **kwargs):
     """Return a map vector corresponding to a lat-long map image."""
     # If image doesn't exist, check for it in `img` directory
     if not os.path.exists(image):
@@ -68,30 +68,54 @@ def image2map(image, lmax=10, **kwargs):
     image_array /= 255.0
 
     # Convert it to a map
-    return array2map(image_array, lmax=lmax, **kwargs)
+    return array2map(image_array, **kwargs)
 
 
-def array2map(image_array, lmax=10, sampling_factor=8, **kwargs):
-    """Return a map vector corresponding to a lat-lon map image array."""
+def array2healpix(image_array, nside=16, max_iter=3, **kwargs):
+    """Return a healpix ring-ordered map corresponding to a lat-lon map image array."""
     if hp is None:
         raise ImportError(
             "Please install the `healpy` Python package to "
             "enable this feature. See `https://healpy.readthedocs.io`."
         )
-    # Figure out a reasonable number of sides
-    # Note that this is not optimized. There may be a better criterion
-    # for figuring out the optimal number of sides.
-    npix = image_array.shape[0] * image_array.shape[1]
-    nside = 2
-    while hp.nside2npix(nside) * sampling_factor < npix:
-        nside *= 2
 
-    # Convert to a healpix map
-    theta = np.linspace(0, np.pi, image_array.shape[0])[:, None]
-    phi = np.linspace(-np.pi, np.pi, image_array.shape[1])[::-1]
-    pix = hp.ang2pix(nside, theta, phi, nest=False)
-    healpix_map = np.zeros(hp.nside2npix(nside), dtype=np.float64)
-    healpix_map[pix] = image_array
+    # Starting value for the zoom
+    zoom = 2
+
+    # Keep track of the number of unseen pixels
+    unseen = 1
+    ntries = 0
+    while unseen > 0:
+
+        # Make the image bigger so we have good angular coverage
+        image_array = ndimage.zoom(image_array, zoom)
+
+        # Convert to a healpix map
+        theta = np.linspace(0, np.pi, image_array.shape[0])[:, None]
+        phi = np.linspace(-np.pi, np.pi, image_array.shape[1])[::-1]
+        pix = hp.ang2pix(nside, theta, phi, nest=False)
+        healpix_map = (
+            np.ones(hp.nside2npix(nside), dtype=np.float64) * hp.UNSEEN
+        )
+        healpix_map[pix] = image_array
+
+        # Count the unseen pixels
+        unseen = np.count_nonzero(healpix_map == hp.UNSEEN)
+
+        # Did we do this too many times?
+        ntries += 1
+        if ntries > max_iter:
+            raise ValueError(
+                "Maximum number of iterations exceeded. Either decreaser `nside` or increase `max_iter`."
+            )
+
+    return healpix_map
+
+
+def array2map(image_array, **kwargs):
+    """Return a map vector corresponding to a lat-lon map image array."""
+    # Get the healpix map
+    healpix_map = array2healpix(image_array, **kwargs)
 
     # Now convert it to a spherical harmonic map
-    return healpix2map(healpix_map, lmax=lmax, **kwargs)
+    return healpix2map(healpix_map, **kwargs)