aa52c36e8ae992ac24dba6de22ecd1145f6126d3
Rodrigo Luger <rodluger@gmail.com>
2020-03-11 20:17:59 -0400

    better test

--- tests/greedy/test_I_stability.py
+++ tests/greedy/test_I_stability.py
@@ -4,20 +4,31 @@ import matplotlib.pyplot as plt
 
 
 def test_I_stability(plot=False):
-    # FB found this unstable limit; I believe the instabilities
-    # come from the upward recursion in the I integral.
+    # FB found this unstable limit. The instability comes
+    # from two places:
+    # 1. The upward recursion in the I integral is unstable.
+    #    We need to either implement a tridiagonal solver as
+    #    in the J integral and/or refine our computation of kappa.
+    # 2. The terminator parameter b = 1, which causes `get_angles` to
+    #    oscillate between different integration codes. When b
+    #    approaches unity, we should switch to the regular starry
+    #    solver, since the terminator is so close to the limb that its
+    #    presence doesn't matter.
     xo, yo, zo, ro = (
         31.03953239062832,
         23.892679948795926,
         1.0,
         39.10406741663172,
     )
+    xs = 0
+    ys = 0
+    zs = 1
     xo = np.linspace(xo - 2, xo + 2, 1000)
 
     # The instability shows up at ydeg ~ 5 and gets bad at ydeg ~ 6
     map = starry.Map(ydeg=6, reflected=True)
     map[6, :] = 1
-    flux1 = map.flux(xo=xo, yo=yo, zo=zo, ro=ro, xs=0, ys=0, zs=1)
+    flux1 = map.flux(xo=xo, yo=yo, zo=zo, ro=ro, xs=xs, ys=ys, zs=zs)
 
     # The flux above should be *exactly* equal to the flux of a
     # linearly-limb darkened source with u_1 = 1.0, since linear
