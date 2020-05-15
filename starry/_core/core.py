fixed inconsistencies in the normalization of reflected maps

starry/_core/core.py
@@ -226,6 +226,17 @@ class OpsYlm(object):
         # Transform to the Ylm basis
         pTA1 = ts.dot(pT, self.A1)
 
+        # NOTE: The factor of `pi` ensures the correct normalization.
+        # This is *different* from the derivation in the paper, but it's
+        # due to the fact that the in starry we normalize the spherical
+        # harmonics in a slightly strange way (they're normalized so that
+        # the integral of Y_{0,0} over the unit sphere is 4, not 4pi).
+        # This is useful for thermal light maps, where the flux from a map
+        # with Y_{0,0} = 1 is *unity*. But it messes up things for reflected
+        # light maps, so we need to account for that here.
+        if self._reflected:
+            pTA1 *= np.pi
+
         # We're done
         return pTA1
 
@@ -892,10 +903,20 @@ class OpsReflected(OpsYlm):
             I = tt.shape_padaxis(I, 1)
 
         # Weight the image by the illumination
+        # NOTE: The factor of `pi` ensures the correct normalization.
+        # This is *different* from the derivation in the paper, but it's
+        # due to the fact that the in starry we normalize the spherical
+        # harmonics in a slightly strange way (they're normalized so that
+        # the integral of Y_{0,0} over the unit sphere is 4, not 4pi).
+        # This is useful for thermal light maps, where the flux from a map
+        # with Y_{0,0} = 1 is *unity*. But it messes up things for reflected
+        # light maps, so we need to account for that here.
         intensity = tt.switch(
             tt.isnan(intensity),
             intensity,
-            ifelse(illuminate, intensity * I, intensity * tt.ones_like(I)),
+            ifelse(
+                illuminate, intensity * I, np.pi * intensity * tt.ones_like(I)
+            ),
         )
 
         return intensity
@@ -935,8 +956,16 @@ class OpsReflected(OpsYlm):
                 ld, tt.dot(self.F(u, f), A1y), tt.dot(self.F(u0, f), A1y)
             )
 
-        # Dot the polynomial into the basis
-        return tt.dot(pT, A1y)
+        # Dot the polynomial into the basis.
+        # NOTE: The factor of `pi` ensures the correct normalization.
+        # This is *different* from the derivation in the paper, but it's
+        # due to the fact that the in starry we normalize the spherical
+        # harmonics in a slightly strange way (they're normalized so that
+        # the integral of Y_{0,0} over the unit sphere is 4, not 4pi).
+        # This is useful for thermal light maps, where the flux from a map
+        # with Y_{0,0} = 1 is *unity*. But it messes up things for reflected
+        # light maps, so we need to account for that here.
+        return np.pi * tt.dot(pT, A1y)
 
     @autocompile
     def X_point_source(
@@ -989,7 +1018,10 @@ class OpsReflected(OpsYlm):
         )
 
         # Weight by the distance to the source.
-        X *= STARRY_REFLECTANCE_FLUX_NORMALIZATION / tt.shape_padright(r2)
+        # NOTE: In the paper, we divide by `pi` at this step. But the way
+        # we coded things up, starry maps are already implicitly pi-normalized
+        # (via the change-of-basis matrix A1), so we don't need to do that here.
+        X /= tt.shape_padright(r2)
 
         # We're done
         return X
@@ -1227,8 +1259,18 @@ class OpsReflected(OpsYlm):
             I = tt.repeat(I, self.nw, 1)
 
         # Weight the image by the illumination
+        # NOTE: The factor of `pi` ensures the correct normalization.
+        # This is *different* from the derivation in the paper, but it's
+        # due to the fact that the in starry we normalize the spherical
+        # harmonics in a slightly strange way (they're normalized so that
+        # the integral of Y_{0,0} over the unit sphere is 4, not 4pi).
+        # This is useful for thermal light maps, where the flux from a map
+        # with Y_{0,0} = 1 is *unity*. But it messes up things for reflected
+        # light maps, so we need to account for that here.
         image = ifelse(
-            illuminate, tt.switch(tt.isnan(image), image, image * I), image
+            illuminate,
+            tt.switch(tt.isnan(image), image, image * I),
+            np.pi * image,
         )
 
         # We need the shape to be (nframes, npix, npix)
@@ -1293,8 +1335,11 @@ class OpsReflected(OpsYlm):
             sigr > 0, ifelse(on94_exact, I_on94_exact, I_on94_approx), I_lamb
         )
 
-        # Weight by the distance to the source and we're done
-        I *= STARRY_REFLECTANCE_FLUX_NORMALIZATION / tt.shape_padleft(r2)
+        # Weight by the distance to the source.
+        # NOTE: In the paper, we divide by `pi` at this step. But the way
+        # we coded things up, starry maps are already implicitly pi-normalized
+        # (via the change-of-basis matrix A1), so we don't need to do that here.
+        I /= tt.shape_padleft(r2)
         return I
 
     @autocompile

starry/maps.py
@@ -38,26 +38,10 @@ __all__ = [
 
 class Amplitude(object):
     def __get__(self, instance, owner):
-        if instance.__props__["reflected"]:
-            return instance._amp * STARRY_REFLECTANCE_FLUX_NORMALIZATION
-        else:
-            return instance._amp
+        return instance._amp
 
     def __set__(self, instance, value):
-        if instance.__props__["reflected"]:
-            # NOTE: For reflected light maps, the internal amplitude `map._amp`
-            # is a factor of `np.pi` larger than the user-facing amplitude
-            # `map.amp`. This helps make the default map albedo unity instead
-            # of `1/np.pi` (which is the default intensity in thermal light).
-            # This is a bit of a hack, and we should reconsider this if it
-            # leads to confusion / issues for users.
-            instance._amp = math.cast(
-                np.ones(instance.nw)
-                * value
-                / STARRY_REFLECTANCE_FLUX_NORMALIZATION
-            )
-        else:
-            instance._amp = math.cast(np.ones(instance.nw) * value)
+        instance._amp = math.cast(np.ones(instance.nw) * value)
 
 
 class MapBase(object):
@@ -293,8 +277,7 @@ class MapBase(object):
         f[0] = np.pi
         self._f = math.cast(f)
 
-        # Let the `Amplitude` class take care of the normalization
-        self.amp = kwargs.pop("amp", np.ones(self.nw))
+        self._amp = math.cast(kwargs.pop("amp", np.ones(self.nw)))
 
         # Reset data and priors
         self._flux = None
@@ -826,10 +809,7 @@ class MapBase(object):
         """
         if mu is None:
             mu = np.zeros(self.Ny)
-            if self.__props__["reflected"]:
-                mu[0] = 1 / STARRY_REFLECTANCE_FLUX_NORMALIZATION
-            else:
-                mu[0] = 1.0
+            mu[0] = 1.0
             mu = math.cast(mu)
         self._mu = math.cast(mu) * math.cast(np.ones(self.Ny))
         self._L = linalg.Covariance(L, cho_L, N=self.Ny)
@@ -882,18 +862,6 @@ class MapBase(object):
         self._solution = linalg.solve(
             X, self._flux, self._C.cholesky, self._mu, self._L.inverse
         )
-
-        # HACK: We need to remove our internal weighting by pi here
-        # for reflected light maps. (It gets added back in automatially
-        # when setting `map.amp` below; see the `Amplitude` class above).
-        # FACT: The *rows* of the lower cholesky factorization of the
-        # poterior covariance scale in the same way as the posterior
-        # mean. See `tests/greedy/test_chol_scaling.py`.
-        if self.__props__["reflected"]:
-            self._solution = (
-                self._solution[0] / np.pi,
-                self._solution[1] / np.pi,
-            )
 
         # Set the amplitude and coefficients
         x, _ = self._solution
@@ -1171,7 +1139,7 @@ class YlmBase(object):
         self._check_kwargs("flux", kwargs)
 
         # Compute & return
-        return self._amp * self.ops.flux(
+        return self.amp * self.ops.flux(
             theta,
             xo,
             yo,
@@ -1223,7 +1191,7 @@ class YlmBase(object):
         self._check_kwargs("intensity", kwargs)
 
         # Compute & return
-        return self._amp * self.ops.intensity(
+        return self.amp * self.ops.intensity(
             lat, lon, self._y, self._u, self._f, alpha_theta, ld
         )
 
@@ -1264,11 +1232,11 @@ class YlmBase(object):
 
         # Compute
         if self.nw is None or config.lazy:
-            amp = self._amp
+            amp = self.amp
         else:
             # The intensity has shape `(nw, res, res)`
             # so we must reshape `amp` to take the product correctly
-            amp = self._amp[:, np.newaxis, np.newaxis]
+            amp = self.amp[:, np.newaxis, np.newaxis]
         image = amp * self.ops.render(
             res,
             projection,
@@ -1366,7 +1334,7 @@ class YlmBase(object):
         # as that of the input image
         y /= 2 * np.sqrt(np.pi)
         self._y = math.cast(y / y[0])
-        self._amp = math.cast(y[0] * np.pi)
+        self.amp = math.cast(y[0] * np.pi)
 
         # Ensure positive semi-definite?
         if force_psd:
@@ -1581,7 +1549,7 @@ class LimbDarkenedBase(object):
         self._check_kwargs("flux", kwargs)
 
         # Compute & return
-        return self._amp * self.ops.flux(xo, yo, zo, ro, self._u)
+        return self.amp * self.ops.flux(xo, yo, zo, ro, self._u)
 
     def intensity(self, mu=None, x=None, y=None):
         r"""
@@ -1613,7 +1581,7 @@ class LimbDarkenedBase(object):
             mu = (1 - x ** 2 - y ** 2) ** 0.5
 
         # Compute & return
-        return self._amp * self.ops.intensity(mu, self._u)
+        return self.amp * self.ops.intensity(mu, self._u)
 
     def render(self, res=300):
         """Compute and return the intensity of the map on a grid.
@@ -1631,7 +1599,7 @@ class LimbDarkenedBase(object):
             animated = False
 
         # Compute
-        image = self._amp * self.ops.render_ld(res, self._u)
+        image = self.amp * self.ops.render_ld(res, self._u)
 
         # Squeeze?
         if animated:
@@ -1994,7 +1962,7 @@ class ReflectedBase(object):
         self._check_kwargs("flux", kwargs)
 
         # Compute & return
-        return self._amp * self.ops.flux(
+        return self.amp * self.ops.flux(
             theta,
             xs,
             ys,
@@ -2058,11 +2026,11 @@ class ReflectedBase(object):
 
         # Get the amplitude
         if self.nw is None:
-            amp = self._amp
+            amp = self.amp
         else:
             # The intensity has shape `(nsurf_pts, nw, nsource_pts)`
             # so we must reshape `amp` to take the product correctly
-            amp = self._amp[np.newaxis, :, np.newaxis]
+            amp = self.amp[np.newaxis, :, np.newaxis]
 
         # If differentially rotating, allow a `theta` keyword
         if self.drorder > 0:
@@ -2171,11 +2139,11 @@ class ReflectedBase(object):
 
         # Compute
         if self.nw is None:
-            amp = self._amp
+            amp = self.amp
         else:
             # The intensity has shape `(nw, res, res)`
             # so we must reshape `amp` to take the product correctly
-            amp = self._amp[:, np.newaxis, np.newaxis]
+            amp = self.amp[:, np.newaxis, np.newaxis]
 
         image = amp * self.ops.render(
             res,
@@ -2221,11 +2189,11 @@ class ReflectedBase(object):
         on94_exact = int(kwargs.pop("on94_exact", False))
 
         if self.nw is None:
-            amp = self._amp
+            amp = self.amp
         else:
             # The intensity has shape `(nw, res, res)`
             # so we must reshape `amp` to take the product correctly
-            amp = self._amp[:, np.newaxis, np.newaxis]
+            amp = self.amp[:, np.newaxis, np.newaxis]
 
         if config.lazy:
             # Evaluate the variables

tests/lazy/test_solve_lazy.py
@@ -109,4 +109,4 @@ def test_lnlike(L, C, woodbury):
 
     # Verify that we get the correct inclination
     assert incs[np.argmax(ll)] == 60
-    assert np.allclose(ll[np.argmax(ll)], 978.174079)  # benchmarked
+    assert np.allclose(ll[np.argmax(ll)], 974.221605)  # benchmarked