3eeaf7cf2a7696fc3ea5181f8b77a864d21d7293
Rodrigo Luger <rodluger@gmail.com>
2021-02-24 12:14:59 -0500

    improve test coverage

--- tests/greedy/test_show_greedy.py
+++ tests/greedy/test_show_greedy.py
@@ -9,6 +9,7 @@ import matplotlib
 
 matplotlib.use("Agg")
 
+import matplotlib.pyplot as plt
 import starry
 import numpy as np
 import os
@@ -20,31 +21,47 @@ def test_show(mp4=False):
     os.remove("tmp.pdf")
     map.show(file="tmp.pdf", projection="rect")
     os.remove("tmp.pdf")
-    if mp4:
-        map.show(theta=np.linspace(0, 360, 10), file="tmp.mp4")
-        os.remove("tmp.mp4")
+    map.show(theta=np.linspace(0, 360, 10), file="tmp.mp4")
+    os.remove("tmp.mp4")
 
 
-def test_show_reflected(mp4=False):
+def test_show_with_figure():
+    map = starry.Map(ydeg=1, udeg=1)
+    fig, ax = plt.subplots(1)
+    map.show(ax=ax, file="tmp.pdf", projection="ortho")
+    os.remove("tmp.pdf")
+
+
+def test_show_moll():
+    map = starry.Map(ydeg=1, udeg=1)
+    map.show(file="tmp.pdf", projection="moll")
+    os.remove("tmp.pdf")
+
+
+def test_show_colorbar():
+    map = starry.Map(ydeg=1, udeg=1)
+    map.show(file="tmp.pdf", projection="ortho", colorbar=True)
+    os.remove("tmp.pdf")
+
+
+def test_show_reflected():
     map = starry.Map(ydeg=1, udeg=1, reflected=True)
     map.show(file="tmp.pdf", projection="ortho")
     os.remove("tmp.pdf")
     map.show(file="tmp.pdf", projection="rect")
     os.remove("tmp.pdf")
-    if mp4:
-        map.show(theta=np.linspace(0, 360, 10), file="tmp.mp4")
-        os.remove("tmp.mp4")
+    map.show(theta=np.linspace(0, 360, 10), file="tmp.mp4")
+    os.remove("tmp.mp4")
 
 
-def test_show_rv(mp4=False):
+def test_show_rv():
     map = starry.Map(ydeg=1, udeg=1, rv=True)
     map.show(rv=True, file="tmp.pdf", projection="ortho")
     os.remove("tmp.pdf")
     map.show(rv=True, file="tmp.pdf", projection="rect")
     os.remove("tmp.pdf")
-    if mp4:
-        map.show(rv=True, theta=np.linspace(0, 360, 10), file="tmp.mp4")
-        os.remove("tmp.mp4")
+    map.show(rv=True, theta=np.linspace(0, 360, 10), file="tmp.mp4")
+    os.remove("tmp.mp4")
 
 
 def test_show_ld():
@@ -53,25 +70,23 @@ def test_show_ld():
     os.remove("tmp.pdf")
 
 
-def test_system_show(mp4=False):
+def test_system_show():
     pri = starry.Primary(starry.Map())
     sec = starry.Secondary(starry.Map(), porb=1.0)
     sys = starry.System(pri, sec)
     sys.show(0.1, file="tmp.pdf")
     os.remove("tmp.pdf")
-    if mp4:
-        sys.show([0.1, 0.2], file="tmp.mp4")
-        os.remove("tmp.mp4")
+    sys.show([0.1, 0.2], file="tmp.mp4")
+    os.remove("tmp.mp4")
     sys.show([0.1, 0.2], file="tmp.gif")
     os.remove("tmp.gif")
 
 
-def test_system_rv_show(mp4=False):
+def test_system_rv_show():
     pri = starry.Primary(starry.Map(rv=True))
     sec = starry.Secondary(starry.Map(rv=True), porb=1.0)
     sys = starry.System(pri, sec)
     sys.show(0.1, file="tmp.pdf")
     os.remove("tmp.pdf")
-    if mp4:
-        sys.show([0.1, 0.2], file="tmp.mp4")
-        os.remove("tmp.mp4")
+    sys.show([0.1, 0.2], file="tmp.mp4")
+    os.remove("tmp.mp4")
