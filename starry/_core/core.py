1e5e90a55a66808588e6b77549497d1713cbb05f
Rodrigo Luger <rodluger@gmail.com>
2021-09-28 16:24:50 -0400

    debug

--- starry/_core/core.py
+++ starry/_core/core.py
@@ -1817,7 +1817,9 @@ class OpsDoppler(OpsYlm):
         sijk = tt.zeros((deg + 1, deg + 1, 2, tt.shape(x)[0]))
 
         # Initial conditions
-        r2 = tt.maximum(1 - x ** 2, tt.zeros_like(x) + 1e-12)
+        r2 = tt.maximum(
+            1 - x ** 2, tt.zeros_like(x) + 1e-100
+        )  # avoid deriv issues at x = 1
         sijk = tt.set_subtensor(sijk[0, 0, 0], 2 * r2 ** 0.5)
         sijk = tt.set_subtensor(sijk[0, 0, 1], 0.5 * np.pi * r2)
 
